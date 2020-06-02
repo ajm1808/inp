82249e218845d8630632cecd919ab25b0d0f514d
ajm1808 <ajm180885@gmail.com>
2020-06-02 12:24:23 +0100

    filled questions into steps

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import ChatBot from 'react-simple-chatbot';
-import Trump from '../img/Trump.jpg';
+import Trumpyes from '../img/trumpyes.jpg';
+import Trumpno from '../img/trumpno.PNG';
 
 // all available props
 const theme = {
@@ -19,7 +20,7 @@ const theme = {
 const steps = [
   {
     id: '1',
-    message: 'Who said these quotes?',
+    message: 'Who said this?',
     trigger: 2,
   },
   {
@@ -30,26 +31,173 @@ const steps = [
   {
     id:'3',
     options:[
-      {value:1,label:'Trump',trigger:'5'},
-      {value:2,label:'Stalin',trigger:'4'},
-      {value:3,label:'Mussolini',trigger:'4'},
-      {value:4,label:'Hitler',trigger:'4'},
+      {value:1,label:'Trump',trigger:'4'},
+      {value:2,label:'Stalin',trigger:'6'},
+      {value:3,label:'Mussolini',trigger:'6'},
+      {value:4,label:'Hitler',trigger:'6'},
     ]
   },
   {
     id:'4',
-    message:'Wrong answer fool!',
-    trigger:'2',
+    component:(
+      <div className="trumpPic">
+        <p>Correct, it was</p>
+        <img id="trumpImg" src={Trumpyes}/>
+      </div>
+    ),
+    trigger:7,
   },
   {
     id:'5',
     component:(
-      <div>
-        <p>Correct, it was</p>
-        <img src={Trump}/>
+      <div className="trumpPic">
+        <p>Not this guy!</p>
+        <img id="trumpImg" src={Trumpno}/>
       </div>
-    )
+    ),
+  },
+  {
+    id:'6',
+    message: "Nope, try again!",
+    trigger:2,
+  },
+  {
+    id:'7',
+    message:"I'll turn my life into a masterpiece",
+    trigger:8,
+  },
+  {
+    id:'8',
+    options:[
+      {value:1,label:'Trump',trigger:'10'},
+      {value:2,label:'Stalin',trigger:'9'},
+      {value:3,label:'Mussolini',trigger:'11'},
+      {value:4,label:'Hitler',trigger:'9'},
+    ]
+  },
+  {
+    id:'9',
+    message: "Nope, try again!",
+    trigger:8,
+  },
+  {
+    id:'10',
+    component:(
+      <div className="trumpPic">
+        <p>Not this guy!</p>
+        <img id="trumpImg" src={Trumpno}/>
+      </div>
+    ),
+    trigger:8,
+  },
+  {
+    id:'11',
+    message: "Correct!",
+    trigger:12,
+  },
+  {
+    id:'12',
+    message:"The world belongs to the man with guts!",
+    trigger:13,
+  },
+  {
+    id:'13',
+    options:[
+      {value:1,label:'Trump',trigger:'15'},
+      {value:2,label:'Stalin',trigger:'14'},
+      {value:3,label:'Mussolini',trigger:'14'},
+      {value:4,label:'Hitler',trigger:'16'},
+    ]
+  },
+  {
+    id:'14',
+    message: "Nope, try again!",
+    trigger:13,
+  },
+  {
+    id:'15',
+    component:(
+      <div className="trumpPic">
+        <p>Not this guy!</p>
+        <img id="trumpImg" src={Trumpno}/>
+      </div>
+    ),
+    trigger:13,
+  },
+  {
+    id:'16',
+    message: "Correct!",
+    trigger:17,
+  },
+  {
+    id:'17',
+    message:"Success is the sole earthly judge of right and wrong",
+    trigger:18,
+  },
+  {
+    id:'18',
+    options:[
+      {value:1,label:'Trump',trigger:'20'},
+      {value:2,label:'Stalin',trigger:'19'},
+      {value:3,label:'Mussolini',trigger:'19'},
+      {value:4,label:'Hitler',trigger:'21'},
+    ]
+  },
+  {
+    id:'19',
+    message:"Nope, try again",
+    trigger:18,
+  },
+  {
+    id:'20',
+    component:(
+      <div className="trumpPic">
+        // <p>Not this guy!</p>
+        <img id="trumpImg" src={Trumpno}/>
+      </div>
+    ),
+    trigger:18,
+  },
+  {
+    id:'21',
+    message:"Correct",
+    trigger:22,
+  },
+  {
+    id:'22',
+    message:"Good people don't go into government",
+    trigger:23,
+  },
+  {
+    id:'23',
+    options:[
+      {value:1,label:'Trump',trigger:'25'},
+      {value:2,label:'Stalin',trigger:'24'},
+      {value:3,label:'Mussolini',trigger:'24'},
+      {value:4,label:'Hitler',trigger:'24'},
+    ]
+  },
+  {
+    id:'24',
+    message:"Nope, try again",
+    trigger:23
+  },
+  {
+    id:'25',
+    component:(
+      <div className="trumpPic">
+        <p>Correct, it was</p>
+        <img id="trumpImg" src={Trumpyes}/>
+      </div>
+    ),
+    trigger:26,
+  },
+  {
+    id:'26',
+    message:"Thanks for playing - remember, democracy dies in darkness",
+    end:true,
   }
+
 
 ];
 
