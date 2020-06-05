8de63e9bccbdacde1171c9960a2ff4cceccc1c51
ajm1808 <ajm180885@gmail.com>
2020-06-05 14:24:42 +0100

    Updates

---
+++ precache-manifest.b44163ec1487deff80b647a273016dc5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9219077a4daccce2c876c7c494236a2f",
+    "url": "/inp/index.html"
+  },
+  {
+    "revision": "4077341086c4dac0e69f",
+    "url": "/inp/static/css/main.4bcc23c8.chunk.css"
+  },
+  {
+    "revision": "f36aee096e9beb4837b2",
+    "url": "/inp/static/js/2.2d1f8cdf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/inp/static/js/2.2d1f8cdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4077341086c4dac0e69f",
+    "url": "/inp/static/js/main.c271321a.chunk.js"
+  },
+  {
+    "revision": "3bcc745abc369a063df4",
+    "url": "/inp/static/js/runtime-main.e6c69c41.js"
+  },
+  {
+    "revision": "4917d7e88df13a7b4df412c34617ccd6",
+    "url": "/inp/static/media/boris.4917d7e8.png"
+  },
+  {
+    "revision": "659c7dd07dc9323bb59db49e5d28b5f9",
+    "url": "/inp/static/media/boris2.659c7dd0.jpg"
+  },
+  {
+    "revision": "fb0f4b4cf6188b35fcbc700827a6b14b",
+    "url": "/inp/static/media/trumpno.fb0f4b4c.PNG"
+  },
+  {
+    "revision": "a59659c6ab20b6497096cd024f3dad8c",
+    "url": "/inp/static/media/trumpyes.a59659c6.jpg"
+  },
+  {
+    "revision": "e120127750d2684684bcd7e475fef2fb",
+    "url": "/inp/static/media/trumpyes.e1201277.png"
+  }
+]);
